eef1f27c74659f5c26a0459cef8d790362dc7cb8
Marcos Junior <markosjr1969@gmail.com>
2021-11-12 19:34:51 -0200

    create multiplyDefault

---
+++ aula 12/multiplyDefault.ts
@@ -0,0 +1,4 @@
+function multiplyDefaut (n1:number,n2:number):number{
+    return n1*n2;
+}
+export default multiplyDefaut;
--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import {show as welcome, sum} from './show'
 const ret = welcome('Marcos')
 console.log(ret) 
 
-const summary = sum(1,2);
-console.log (summary)
+//const summary = sum(1, 2);
+//console.log (summary)
 
